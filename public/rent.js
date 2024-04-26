1de6de70c1863239ba18264969589b3f0c993b35
Захар Чертухин <zaharchertuhin@gmail.com>
2024-04-26 18:13:10 +0400

    adaptive edit 3 + equipment

---
+++ public/rent.js
@@ -0,0 +1,27 @@
+window.onload = function() {
+  // Создаем объект XMLHttpRequest
+  var xhttp = new XMLHttpRequest();
+
+  // Открываем запрос на получение данных из файла
+  xhttp.open("GET", "equipment.xml", true);
+
+  // Устанавливаем обработчик события загрузки
+  xhttp.onload = function() {
+// Проверим статус ответа
+ if (xhttp.status >= 200 && xhttp.status < 300) {
+ // Если статус успешный, выводим содержимое файла в элемент <div id="container">
+ document.getElementById("container").innerHTML = xhttp.responseText;
+ } else {
+ // Если статус неуспешный, выводим сообщение об ошибке
+ document.getElementById("container").innerHTML = "Ошибка загрузки файла";
+ }
+ };
+
+    xhttp.onerror = function() {
+      // В случае ошибки выводим сообщение об ошибке
+      document.getElementById("container").innerHTML = "Ошибка загрузки файла";
+    };
+
+// Отправляем запрос
+    xhttp.send();
+}
--- server/app.js
+++ server/app.js
@@ -1,18 +1,21 @@
-
 require('dotenv').config(); // Подключаем dotenv
+
+
+const csv = require('csv-parser');
+const fs = require('fs');
 
 const TelegramBot = require('node-telegram-bot-api');
 const express = require('express');
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 const bodyParser = require('body-parser');
 
-const app = express();
+var app = express();
 const port = process.env.PORT;
 const botToken = process.env.TELEGRAM_BOT_TOKEN;
 const chatId = process.env.TELEGRAM_CHAT_ID;
 
 
-const bot = new TelegramBot(botToken, { polling: false });
+const bot = new TelegramBot(botToken, {polling: false});
 
 app.use(bodyParser.json());
 const urlencodedParser = express.urlencoded({extended: false});
@@ -20,7 +23,7 @@ const urlencodedParser = express.urlencoded({extended: false});
 app.use(express.static('public'));
 
 app.post('/contact', urlencodedParser, function (req, res) {
-  const { Name, Email, Message } = req.body;
+  const {Name, Email, Message} = req.body;
 
 
   if (Name === undefined || Email === undefined || Message === undefined) {
@@ -40,6 +43,6 @@ app.post('/contact', urlencodedParser, function (req, res) {
     });
 });
 
-app.listen(port, '0.0.0.0',() => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Сервер запущен на порту ${port}`);
 });
