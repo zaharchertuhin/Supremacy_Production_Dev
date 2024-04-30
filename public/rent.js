0afcea1f4ff71e4a33de0d93ad19974ec2c99854
Захар Чертухин <zaharchertuhin@gmail.com>
2024-04-30 15:33:32 +0400

    Adaptive edit 4

--- public/rent.js
+++ public/rent.js
@@ -1,27 +1,15 @@
-window.onload = function() {
-  // Создаем объект XMLHttpRequest
+window.onload = function () {
   const xhttp = new XMLHttpRequest();
-
-  // Открываем запрос на получение данных из файла
   xhttp.open("GET", "data", true);
-
-  // Устанавливаем обработчик события загрузки
-  xhttp.onload = function() {
-// Проверим статус ответа
- if (xhttp.status >= 200 && xhttp.status < 300) {
- // Если статус успешный, выводим содержимое файла в элемент <div id="container">
- document.getElementById("container").innerHTML = xhttp.responseText;
- } else {
- // Если статус неуспешный, выводим сообщение об ошибке
- document.getElementById("container").innerHTML = "Ошибка загрузки файла";
- }
- };
-
-    xhttp.onerror = function() {
-      // В случае ошибки выводим сообщение об ошибке
+  xhttp.onload = function () {
+    if (xhttp.status >= 200 && xhttp.status < 300) {
+      document.getElementById("container").innerHTML = xhttp.responseText;
+    } else {
       document.getElementById("container").innerHTML = "Ошибка загрузки файла";
-    };
-
-// Отправляем запрос
-    xhttp.send();
+    }
+  };
+  xhttp.onerror = function () {
+    document.getElementById("container").innerHTML = "Ошибка загрузки файла";
+  };
+  xhttp.send();
 }
